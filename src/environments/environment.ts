a24c734a000e58daee565c602abcf04b7a585b17
jorge <jsdelduca@gmail.com>
2020-09-18 16:20:52 -0400

    changes to environment

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-
-export const environment = {
-    production: false,
-    PRODUCTS_API: "http://localhost:4200/api/products",
-    CLIENTS_API: "http://localhost:4200/api/clients",
-    CONFIG_API: "http://localhost:4200/api/currency-change",
-};
