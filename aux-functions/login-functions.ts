3a90aea17a5c08bc71497a4c4c0b1f57aa2bc829
jorge <jsdelduca@gmail.com>
2021-02-01 12:34:38 -0400

    check

--- aux-functions/login-functions.ts
+++ aux-functions/login-functions.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 
 
-export const isLoggedIn = (req: Request): boolean => {
+export const isLoggedIn = (req: any): boolean => {
     return !!req.session!.userId
 
 }
@@ -12,7 +12,7 @@ export const isLoggedIn = (req: Request): boolean => {
 
 
 export const logOut = (req: Request, res: Response) => {
-    new Promise((resolve, reject) => {
+    new Promise<void>((resolve, reject) => {
         req.session!.destroy((err) => {
             if(err) {
                 reject(err)
