a03d7fb4bdbfa8f6cbb65eaa3dc15cb2de48ea40
jorge <jsdelduca@gmail.com>
2021-02-22 02:46:09 -0400

    redis config updated

--- server.ts
+++ server.ts
@@ -68,7 +68,7 @@ export function app() {
   server.use(parser.json());
 
 
- const redisClient = redis.createClient();
+ const redisClient = redis.createClient(process.env.REDIS_URL);
 
 
   var redisStore = connectRedis(session)
